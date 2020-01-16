91c06774c1cbdde1f9dd0bf184734056a390970a
AnijitSau <2anijit@gmail.com>
2020-01-16 17:18:24 +0530

    backend is updated with the solidity files

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract TicTacToe {
+
+  // initialize the box as 3 x 3 Array of unsigned integers    
+  uint [3][3] box;    
+  
+  // initialize the box when the contract is deployed
+  constructor() public {
+      initializeBox();
+  }
+  
+  
+  // initialize the box the O values for each cell
+  function initializeBox() public {
+      /* this section can be written using for loop however for simplicity and 
+         gas consumption it is omitted 
+      */
+      
+      box = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  }
+  
+  // save current move by user / computer  
+  function saveCurrentMove(uint rowVal, uint columnVal, uint move) public {
+      box[rowVal][columnVal] = move;
+  }
+  
+  // get the current box
+  function getCurrentBox() public view returns(uint [3][3] memory) {
+      return box;
+  }
+  
+}
